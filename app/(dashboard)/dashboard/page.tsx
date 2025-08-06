39db7cc6c9ab71eed1ddc721f0e0cea2a7c2d38a
Sridharjeyakumar <sridhardk7@gmail.com>
2025-08-06 16:43:11 +0530

    working on url for availed block

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -73,7 +73,7 @@ const hasInProgressBlock = requestsData?.data?.requests?.find(
           <a href="/create-block-request" className="w-full rounded-full bg-[#eeb8f7] border border-black py-6 text-xl font-extrabold text-black text-center shadow hover:scale-105 transition">ENTER NEW BLOCK REQUEST</a>
           <a href="/edit-request" className="w-full rounded-full bg-[#aee6f7] border border-black py-6 text-xl font-extrabold text-black text-center shadow hover:scale-105 transition">EDIT/CANCEL PREVIOUS BLOCK REQUESTS</a>
           <a href="/request-table" className="w-full rounded-full bg-[#c7c7f7] border border-black py-6 text-xl font-extrabold text-black text-center shadow hover:scale-105 transition">SUMMARY OF MY BLOCK REQUESTS</a>
-          <a href={`rbms://app?cugNumber=${session?.user?.phone}`} className="w-full rounded-full bg-[#a6f7a6] border border-black py-6 text-xl font-extrabold text-black text-center shadow hover:scale-105 transition">AVAIL BLOCK AT SITE</a>
+          <a href={`rbms://app?cugNumber=${session?.user?.phone}&token=W1IU66ZFEBFBF6C1dGmouN6PVyHARQJg`} className="w-full rounded-full bg-[#a6f7a6] border border-black py-6 text-xl font-extrabold text-black text-center shadow hover:scale-105 transition">AVAIL BLOCK AT SITE</a>
 
 {hasInProgressBlock && (
   <a
@@ -252,29 +252,29 @@ if (session?.user?.role === "SM") {
     );
   }
   if (session?.user?.role === "JE") {
-//   const handleClick = async () => {
-//   if (!session?.user?.phone) {
-//     alert("Phone number not available");
-//     return;
-//   }
+  const handleClick = async () => {
+  if (!session?.user?.phone) {
+    alert("Phone number not available");
+    return;
+  }
 
-//   try {
-//     const response = await axiosInstance.get("/api/user-request/manager-cug", {
-//       params: { cugNumber: session.user.phone },
-//     });
+  try {
+    const response = await axiosInstance.get("/api/user-request/manager-cug", {
+      params: { cugNumber: session.user.phone },
+    });
 
-//     const managerCug = response.data?.data?.managerPhone;
+    const managerCug = response.data?.data?.managerPhone;
 
-//     if (managerCug) {
-//       window.location.href = `rbms://app?cugNumber=${managerCug}`;
-//     } else {
-//       alert("Manager CUG number not found");
-//     }
-//   } catch (error) {
-//     console.error("Error fetching manager phone:", error);
-//     alert("Something went wrong while fetching manager CUG.");
-//   }
-// };
+    if (managerCug) {
+      window.location.href = `rbms://app?cugNumber=${managerCug}&token=W1IU66ZFEBFBF6C1dGmouN6PVyHARQJg`;
+    } else {
+      alert("Manager CUG number not found");
+    }
+  } catch (error) {
+    console.error("Error fetching manager phone:", error);
+    alert("Something went wrong while fetching manager CUG.");
+  }
+};
     return (
       <div className="min-h-screen w-full flex flex-col items-center bg-[#fffbe9]">
         {/* Header */}
@@ -298,19 +298,19 @@ if (session?.user?.role === "SM") {
         </div>
         {/* Navigation buttons */}
         <div className="flex flex-col gap-8 mt-8 w-full max-w-md items-center">
-          <a href={`rbms://app?cugNumber=${session?.user?.phone}`}>
+          {/* <a href={`rbms://app?cugNumber=${session?.user?.phone}&token=W1IU66ZFEBFBF6C1dGmouN6PVyHARQJg`}>
 
             <button className="w-72 bg-[#E6E6FA] py-6 rounded-2xl border-4 border-black text-2xl font-bold text-[#13529e] shadow-lg hover:bg-[#B57CF6] hover:text-white transition-colors">
               AVAIL BLOCK AT SITE
             </button>
-          </a>
+          </a> */}
 
-          {/* <button
+          <button
       onClick={handleClick}
       className="w-72 bg-[#E6E6FA] py-6 rounded-2xl border-4 border-black text-2xl font-bold text-[#13529e] shadow-lg hover:bg-[#B57CF6] hover:text-white transition-colors"
     >
       AVAIL BLOCK AT SITE
-    </button> */}
+    </button>
         </div>
         {/* Logout button */}
         <div className="w-full flex justify-center mt-10 mb-4">
