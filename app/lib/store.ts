3b30ff9b2d819aeab6e5810fceac45e0e16d8308
jeromeignatius14 <159233698+jeromeignatius14@users.noreply.github.com>
2025-08-14 06:52:36 +0000

    updated store.ts

--- app/lib/store.ts
+++ app/lib/store.ts
@@ -1180,6 +1180,12 @@ export const streamData = {
     "both direction affected": ["Rd 1", "Rd 2", "Rd 3", "Rd 4"],
     "both direction not affected": []
   },
+    "JOKATTE-YD": {
+    "up direction affected": [],
+    "down direction affected": [],
+    "both direction affected": ["Rd 1", "Rd 2", "Rd 3", "Rd 4"],
+    "both direction not affected": []
+  },
   "PNMB-YD": {
     "up direction affected": [],
     "down direction affected": [],
