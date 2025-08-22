2c4d146bdc514ef273449815c8aed3692fac2162
Sridharjeyakumar <sridhardk7@gmail.com>
2025-08-22 16:33:24 +0530

    fix the issues

--- app/service/api/manager.ts
+++ app/service/api/manager.ts
@@ -33,6 +33,9 @@ export interface UsersResponse {
 }
 
 export interface UserRequest {
+    sntDisconnectionAssignTo: string;
+    powerBlockDisconnectionAssignTo: string;
+    emergencyBlockRemarks: any;
     overallStatus: string;
     overAllStatus: string;
     divisionId: string;
