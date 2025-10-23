b0574a0d20908c844be70e1a4b80437825004710
Sridharjeyakumar <sridhardk7@gmail.com>
2025-10-23 11:00:26 +0530

    working on availed time

--- app/service/query/user-request.ts
+++ app/service/query/user-request.ts
@@ -17,6 +17,8 @@ export interface RequestResponse {
 }
 
 export interface RequestItem {
+    AvailedTimeTo: string;
+    AvailedTimeFrom: string;
     overAllStatus: String;
     emergencyBlockRemarks : string;
     powerBlockDisconnectionAssignTo: string;
