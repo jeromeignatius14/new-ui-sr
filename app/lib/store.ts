e4cf8a2f391f4eef6034068144743b3d7553f20d
jeromeignatius14 <159233698+jeromeignatius14@users.noreply.github.com>
2025-11-06 13:11:08 +0000

    UPDATED STORE.TS

--- app/lib/store.ts
+++ app/lib/store.ts
@@ -5335,9 +5335,9 @@ export const depotOnLocation = {
   TPJ: [
     "AMM", "ADT", "AGM", "AGX", "ALK", "ALP", "ANP", "ATQ", "ALU", "ARV",
      "AYI", "BAL", "CDM", "CBU", "CUPJ", "ICG", "KLGM", "KLGM", "KDMD", "KNB", 
-     "KMM", "KIK", "KII", "KVL", "CLN", "KDE", "KU", "KMU", "KJPD", "KTM", "LLI",
+     "KMM", "KIK", "KII", "KVL", "CLN", "KDE", "KU", "KMU", "KJPD", "KTM","KIKP NO", "LLI",
       "MMP", "MQ", "MTUR", "MV", "MBU", "NGT", "NCR", "NNM", "NPM", "NVL", "NMJ", 
-      "OTK", "PDV", "PRT", "PRKL", "PKT", "PEM", "PVN", "PYK", "PRL", "GOC", "PUC", 
+      "OTK", "PDV","PVI", "PRT", "PRKL", "PKT", "PEM", "PVN", "PYK", "PRL", "GOC", "PUC", 
       "PDY", "PMB", "PVN", "SMM", "SNDI", "SXR", "SLTH", "SY", "SGM", "SRGM", "SPL",
        "TLNR", "TNI", "TJ", "TUY", "TVR", "TAM", "TP", "TPJ", "TRK", "TXR", "TNK", "TDPR",
         "TTP", "TNM", "TVNL", "TRB", "TT", "TJM", "ULU", "UMG", "VLU", "VDL", "VLDE", "VLNK", "VLR", 
