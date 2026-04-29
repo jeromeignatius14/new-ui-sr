d62f29a26b0c58ddde2b17dbc8e1953233f81f63
Sridhar J <sridharjeyakumar@gmail.com>
2026-04-29 13:53:23 +0530

    adding the remarks

--- app/(dashboard)/(manager)/manage/block-summary/page.tsx
+++ app/(dashboard)/(manager)/manage/block-summary/page.tsx
@@ -5942,6 +5942,7 @@ const handleDownloadDepartmentCount = () => {
                   <th className="border-2 border-black px-1 md:px-2 py-2">Sanctioned time</th>
                   <th className="border-2 border-black px-1 md:px-2 py-2">Availed time</th>
                   <th className="border-2 border-black px-1 md:px-2 py-2">Status</th>
+                  <th className="border-2 border-black px-1 md:px-2 py-2">Remarks</th>
                 </tr>
               </thead>
               <tbody>
@@ -6038,6 +6039,9 @@ const handleDownloadDepartmentCount = () => {
                           style={statusStyle}
                         >
                           {statusLabel}
+                        </td>
+                        <td className="border-2 border-black px-1 md:px-2 py-2 text-black text-[10px] md:text-[14px]">
+                          {block.requestremarks || "-"}
                         </td>
                       </tr>
                     );
